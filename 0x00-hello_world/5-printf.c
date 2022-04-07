5e92d92e8911cdb1ff60bb9cbf019b472b9b641e
Al-hussary1 <Al-hussary@outlook.com>
2022-04-07 14:48:54 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*Return: Return 0 (Success)
-*/
+ *main - Entry point
+ *
+ *Return: Return 0 (Success)
+ */
 int main(void)
 {
-prinf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
