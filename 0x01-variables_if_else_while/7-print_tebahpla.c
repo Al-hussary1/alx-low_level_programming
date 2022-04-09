499ace5e183a11eda7e926c32514043c10432296
Al-hussary1 <Al-hussary@outlook.com>
2022-04-09 11:49:19 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Enty Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 122; alphabet >= 97; alphabet--)
+	{
+		putchar (alphabet);
+	}
+	putchar ('\n');
+return (0);
+}
