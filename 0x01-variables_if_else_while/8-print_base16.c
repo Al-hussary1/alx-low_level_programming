d09c3e3fed73be2ace6ddbc018da0ff89bded85d
Al-hussary1 <Al-hussary@outlook.com>
2022-04-09 13:32:43 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char base_16;
+
+	for (base_18 = 48; base_16 <= 57; base_16++)
+	{
+		putchar (base_16);
+	}
+	for (base_16 = 98; base_16 <= 102; base_16++)
+	{
+		putchar (base_16);
+	}
+	putchar ('\n');
+return (0);
+}
