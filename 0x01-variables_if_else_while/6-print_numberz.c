dcc349f2b98942eff7281a2dfd71b6811ce91ce4
Al-hussary1 <Al-hussary@outlook.com>
2022-04-09 09:26:15 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit <= 57; digit++)
+	{
+		putchar(digit);
+	}
+	putchar("\n");
+return (0);
+}
