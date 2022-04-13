a8ed56da5aaddb8033d9fbd352b28f8a2b1806c7
Al-hussary1 <Al-hussary@outlook.com>
2022-04-13 03:34:51 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,11 @@ void print_alphabet_x10(void)
 	int alphabet;
 	int count;
 
-
 	count = 0;
 	while (count < 10)
 	{
 		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-		{	
+		{
 			_putchar(alphabet);
 		}
 
