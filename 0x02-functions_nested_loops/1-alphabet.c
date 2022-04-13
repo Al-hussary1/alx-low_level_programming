9bafb3553c79dcfc6b84c8c53a1d53feabdd1d3d
Al-hussary1 <Al-hussary@outlook.com>
2022-04-13 03:01:33 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry Point
+ * print_alphabet - print alphabet lowercase
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	char letter;
