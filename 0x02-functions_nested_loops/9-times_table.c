db273e0a97391cf10b67b111a6bbdeada4004d37
Al-hussary1 <Al-hussary@outlook.com>
2022-04-14 07:35:29 -0700

    Task 11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,7 @@ void times_table(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			c = a * b;
-			
+			c = a * b;			
 			if ((c / 10) == 0)
 			{
 				if (b == 0)
@@ -37,8 +36,7 @@ void times_table(void)
 			else
 			{
 				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				
+				_putchar ((c % 10) + '0');			
 				if (b < 9)
 				{
 					_putchar(',');
@@ -46,6 +44,6 @@ void times_table(void)
 				}
 			}
 		}
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
